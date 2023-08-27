c225f6f7717e1cb1a251f31ba7a5da90202705c7
Yury Korziuk <yurko@tut.by>
2023-08-27 20:17:46 +0300

    commit 3. Add lombok to the project. Add annotations to models

--- app/src/main/java/by/korziuk/model/Bank.java
+++ app/src/main/java/by/korziuk/model/Bank.java
@@ -1,7 +1,14 @@
 package by.korziuk.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Bank implements Serializable {
 
     private Long id;
--- app/src/main/java/by/korziuk/model/Transaction.java
+++ app/src/main/java/by/korziuk/model/Transaction.java
@@ -1,9 +1,16 @@
 package by.korziuk.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction implements Serializable {
 
     private Long id;
